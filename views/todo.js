3e343bd1354b914317e701bcbd5d855f6adccebf
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-14 17:12:17 +0700

    Add Terminal table

--- views/todo.js
+++ views/todo.js
@@ -1,30 +1,48 @@
-const chalk = require('chalk');
+const chalk = require('chalk')
+const table = require('terminal-table')
+const t = new table({
+    borderStyle: 0,
+    border: {
+        sep: "║",
+        topLeft: "╔", topMid: "╦", top: "═", topRight: "╗",
+        midLeft: "╠", midMid: "╬", mid: "═", midRight: "╣",
+        botLeft: "╚", botMid: "╩", bot: "═", botRight: "╝"
+    }
+});
+
 
 class Todo {
     static printHelp() {
+        t.push(['WELCOME TO TODO JS APPLICATION BY ANDREW'])
+        console.log("" + t);
         console.log(`$ node todo.js help`)
     }
     static printCommand() {
-        console.log(`$ node todo.js help # menampilkan command apa saja yang tersedia
-$ node todo.js list # Melihat daftar TODO
-$ node todo.js add <task_content> # Menambahkan TODO ke dalam list
-$ node todo.js findById <task_id> # Melihat detail TODO sesuai 'task_id' nya
-$ node todo.js delete <task_id> # Menghapus TODO sesuai 'task_id' nya
-$ node todo.js complete <task_id> # Menandai status TODO selesai
-$ node todo.js uncomplete <task_id> # Menandai status TODO belum selesai
-$ node todo.js list:created asc|desc`
-
+        t.push(
+            ['Command', 'Detail'],
+            [''],
+            ['$ node todo.js help', '# menampilkan command apa saja yang tersedia'],
+            ['$ node todo.js list', '# Melihat daftar'],
+            ['$ node todo.js add <task_content>', '# Menambahkan TODO ke dalam list'],
+            ['$ node todo.js findById <task_id>', '# Melihat detail TODO sesuai \'task_id\' nya'],
+            ['$ node todo.js delete <task_id>', '# Menghapus TODO sesuai \'task_id\' nya'],
+            ['$ node todo.js complete <task_id>', '# Menandai status TODO selesai'],
+            ['$ node todo.js uncomplete <task_id>', '# Menandai status TODO belum selesai'],
         )
+        console.log("" + t)
     }
 
     static printList(list) {
+        t.push(['ID', 'TASK', 'CREATED DATE', 'COMPLETE DATE', 'TAG'], [''])
         for (var key in list) {
             if (list[key].is_complete === true) {
-                console.log(`${list[key].id}. ${chalk.green(`${list[key].task}`)} ${list[key].created_date}`)
-            } else {
-                console.log(`${list[key].id}. ${chalk.red(`${list[key].task}`)} ${list[key].created_date} `)
+                t.push([list[key].id, chalk.green(list[key].task), list[key].created_date, list[key].completed_date, list[key].tag])
+            }
+            else {
+                t.push([list[key].id, chalk.red(list[key].task), list[key].created_date, list[key].completed_date, list[key].tag])
             }
         }
+        console.log("" + t)
     }
 
     static printAddedTodo(task) {
