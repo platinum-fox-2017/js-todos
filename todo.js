42a8f18c143d9186bf94058b7d8e8323d589d979
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-14 11:47:49 +0700

    Release 1

--- todo.js
+++ todo.js
@@ -1,27 +1,48 @@
+const fs = require('fs');
+
 class TodoModel{
     constructor(){
+        this.file = JSON.parse(fs.readFileSync('./data.json', 'UTF-8'));
+    }
 
+    commandList(){
+        let commands = ['', 'help', 'list', 'add <task_content>', 'findById <task_id>', 'delete <task_id>', 'complete <task_id>', 'uncomplete <task_id>'];
+        return commands;
+    } 
+    readFile(){
+        return this.file;
     }
 }
 
 class TodoView{
-    
-    static help(){
-        let commands = ['', 'help', 'list', 'add <task_content>', 'findById <task_id>', 'delete <task_id>', 'complete <task_id>', 'uncomplete <task_id>']
-        let result = []
+
+    static help(commands){
+        let commandList = []
         for (let i=0; i<commands.length; i++){
-            result.push(`node todo.js ${commands[i]}`)
+            commandList.push(`node todo.js ${commands[i]}`)
         }
-        return result.join('\n')
+        return commandList.join('\n')
     }
-    
+
+    static list(file){
+        let task = [];
+        for(let i=0; i<file.length; i++){
+            task.push(`${i+1}. ${file[i].task}`)
+        }
+
+        return task.join('\n');
+    }
 }
 
 class TodoController{
+    
     static run(command, id){
+        let model = new TodoModel();
         switch(command){
             case undefined :
-            case 'help' : return TodoView.help(); break;
+            case 'help' : return TodoView.help(model.commandList()); break;
+            case 'list' : return TodoView.list(model.readFile()); break;
+
         }
     }
 }
