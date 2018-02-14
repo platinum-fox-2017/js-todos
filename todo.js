6f08e815fb0a7c7aa9e7dd822bd1078ea8cf4644
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-14 10:43:48 +0700

    Release 0: Implement the help command

--- todo.js
+++ todo.js
@@ -1,1 +1,15 @@
+var input_argv = process.argv;
+if (!input_argv[2] || input_argv[2] == 'help')
+	console.log(
+		`Usage: node "JS Todos.js" <command>
 
+where <command> is one of:
+	help			menampilkan command apa saja yang tersedia
+	list			melihat daftar TODO
+	add <task_content>	menambahkan TODO ke dalam list
+	findById <task_id>	melihat detail TODO sesuai \`task_id\` nya
+	delete <task_id>	menghapus TODO sesuai \`task_id\` nya
+	complete <task_id>	menandai status TODO selesai
+	uncomplete <task_id>	menandai status TODO belum selesai
+		`
+		);
