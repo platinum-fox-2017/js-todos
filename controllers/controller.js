62cbac1752f732a0191e807bb9fef694e4d83681
srohimah <sitirohimach@gmail.com>
2018-02-14 20:15:07 +0700

    release 2

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,8 +10,8 @@ class Controller{
                 let list = Model.readList();
                 View.showList(list); break;
             case 'add':
-                Model.addContent(command);
-                View.addData(command); break;
+                Model.addContent(command, View.addData);
+               break;
             case 'findById':
                 let id = Model.findById(command);
                 View.showById(id); break;
@@ -24,8 +24,23 @@ class Controller{
             case 'uncomplete':
                 let uncomplete = Model.uncompleteById(command);
                 View.showList(uncomplete); break;
+            case 'list:created':
+                let created = Model.sortByCreated();
+                View.showList(created); break;
+            case 'list:outstanding':
+                let outstanding = Model.outstanding(command);
+                View.showList(outstanding); break;
+            case 'list:completed':
+                let completed = Model.complete();
+                View.showCompletedTask(completed); break;
+            case 'tag':
+                let tag = Model.tag(command[0], command.slice(2));
+                View.showTagedTask(tag,command[0]); break;
+            case 'filter':
+                let filter = Model.filterTag(command);
+                View.showFilter(filter); break;
             default :
-                console.log('inputan salah')
+                View.showHelp(); break;
         }
 
     }
--- views/view.js
+++ views/view.js
@@ -1,4 +1,15 @@
 const chalk = require('chalk');
+const Table = require('cli-table');
+
+let table = new Table({
+    head: ['NO', 'STATUS', 'TO DO']
+  , colWidths: [5, 10, 20]
+});
+let table2 = new Table({
+    head: ['NO', 'STATUS', 'TAG']
+  , colWidths: [5, 10, 20]
+});
+
 class View{
     static showHelp(){
         console.log(chalk.blue('node todo.js'))
@@ -6,28 +17,55 @@ class View{
         console.log(chalk.blue('node todo.js list'))
         console.log(chalk.yellow('node todo.js add <task_content>'))
         console.log(chalk.yellow('node todo.js findById <task_id>'))
-        console.log(chalk.green('node todo.js delete <task_id>'))
+        console.log(chalk.yellow('node todo.js delete <task_id>'))
         console.log(chalk.green('node todo.js complete <task_id>'))
         console.log(chalk.green('node todo.js uncomplete <task_id>'))
+        console.log(chalk.green('node todo.js created'))
+        console.log(chalk.magentaBright('node todo.js outstanding'))
+        console.log(chalk.magentaBright('node todo.js outstanding asc'))
+        console.log(chalk.magentaBright('node todo.js outstanding desc'))
+        console.log(chalk.green('node todo.js completed <task_id>'))
+        console.log(chalk.green('node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>'))
+        console.log(chalk.green('node todo.js filter <tag_name>'))
     }
     static showList(data){
         for (let i = 0; i<data.length; i++){
             if(data[i].status === true){
-                console.log(`${i+1}. [x] ${data[i].task}`)              
+                table.push([i+1, `[x]`, chalk.blue(data[i].task)])              
             }else{
-                console.log(`${i+1}. [ ] ${data[i].task}`)                             
+                table.push([i+1, `[ ]`, chalk.red(data[i].task)])                                 
             }
         }
+        console.log(table.toString());
     }
     static addData(task){
         console.log(`Added "${task}" to your TODO list`)
     }
     static showById(id){
-        console.log(`${id}`)
+        if(id !== ''){
+            console.log(`${id}`)            
+        }else{
+            console.log('id tidak ditemukan')
+        }
     }
     static deleteById(task){
-        task !='' ? console.log(`Deleted "${task}" from your TODO list`) : 'id tidak ditemukan'
-        
+        console.log(`Deleted "${task}" from your TODO list`)
+    }
+    static showCompletedTask(completedTask){
+        for (let i = 0; i<completedTask.length; i++){
+                console.log(`${i+1}. [x] ${completedTask[i]}`)              
+        }
+    }
+    static showTagedTask(data, num){
+        let index = Number(num) 
+        console.log(`Tagged task "${data[index-1].task}" with tag: ${data[index-1].tag}`)
+    }
+
+    static showFilter(data){
+        for (let i = 0; i<data.length; i++){
+            table2.push([i+1, data[i].task, [data[i].tag]])
+        }
+        console.log(table2.toString());
     }
 }
 
