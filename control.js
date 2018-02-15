86ec1fb7fdf2481a4207e26e1c89b9874285d562
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-15 13:31:13 +0700

    Release 2: 4. Implement filter command (Revisi 2)

--- control.js
+++ control.js
@@ -21,35 +21,55 @@ class TODOControls {
 		if (this.command) {
 			switch (this.command.split(':')[0]) {
 				case 'list': {
-					TODOControls.list(this.command.split(':')[1], this.option);
+					TODOModels.list(this.command.split(':')[1], this.option, function (dataObj) {
+						TODOViews.showListMessage(dataObj);
+					});
 					break;
 				}
 				case 'add' : {
-					TODOControls.add(this.option);
+					TODOModels.add(this.option, function (dataObj) {
+						TODOViews.showMessage(`Added ${dataObj[dataObj.length - 1].todo} to your TODO list...`);
+					});
 					break;
 				}
 				case 'findById' : {
-					TODOControls.findById(this.option);
+					TODOModels.findById(this.option, function (dataObj) {
+						TODOViews.showMessage(`${dataObj[i].id}. ${dataObj[i].todo}`);
+					});
 					break;
 				}
 				case 'delete' : {
-					TODOControls.delete(this.option);
+					TODOModels.delete(this.option, function (deletedData) {
+						TODOViews.showMessage(`deleted ${deletedData} from your TODO list...`);
+					});
 					break;
 				}
 				case 'complete' : {
-					TODOControls.complete(this.option);
+					TODOModels.complete(this.option, function () {
+						TODOModels.list(null, null, function (dataObj) {
+							TODOViews.showListMessage(dataObj);
+						})
+					});
 					break;
 				}
 				case 'uncomplete' : {
-					TODOControls.uncomplete(this.option);
+					TODOModels.uncomplete(this.option, function () {
+						TODOModels.list(null, null, function (dataObj) {
+							TODOViews.showListMessage(dataObj);
+						})
+					});
 					break;
 				}
 				case 'tag' : {
-					TODOControls.tag(this.option);
+					TODOModels.tag(this.option, function (name_task, tags) {
+						TODOViews.showMessage(`Tagged task "${name_task}" with tags: ${tags.join(' ')}`);
+					});
 					break;
 				}
 				case 'filter' : {
-					TODOControls.filter(this.command.split(':')[1]);
+					TODOModels.filter(this.command.split(':')[1], function (result) {
+						TODOViews.showListMessage(result);
+					});
 					break;
 				}
 				default :
@@ -77,171 +97,6 @@ where <command> is one of:
 	complete <task_id>	menandai status TODO selesai
 	uncomplete <task_id>	menandai status TODO belum selesai`
 			);
-	}
-
-	static list(option, sort) {
-		TODOModels.readFile(function (dataObj) {
-			switch (option) {
-				case 'created' : {
-					TODOControls.sort(dataObj, sort, 'created_date');
-					TODOViews.showListMessage(dataObj);
-					break;
-				}
-				case 'completed' : {
-					let dataObjCompleted = [];
-					for (let i = 0; i < dataObj.length; i++) {
-						if (dataObj[i].completed_date != '' && dataObj[i].status) dataObjCompleted.push(dataObj[i]);
-					}
-					TODOControls.sort(dataObjCompleted, sort, 'completed_date')
-					TODOViews.showListMessage(dataObjCompleted);
-					break;
-				}
-				default : TODOViews.showListMessage(dataObj);
-			}
-		});
-	}
-
-	static add(data) {
-		TODOModels.readFile(function (dataObj) {
-			let newObj = {
-				id : (Number(dataObj[dataObj.length - 1].id) + 1).toString(),
-				todo : data,
-				status : false,
-				created_date : new Date(),
-				completed_date : '',
-				tag : []
-			};
-
-			dataObj.push(newObj);
-			TODOModels.writeFile(dataObj, function() {
-				TODOViews.showMessage(`Added ${dataObj[dataObj.length - 1].todo} to your TODO list...`);
-			});
-		});
-	}
-
-	static findById(id) {
-		TODOModels.readFile(function (dataObj) {
-			for (let i = 0; i < dataObj.length; i++) {
-				if (dataObj[i].id == id) {
-					TODOViews.showMessage(`${dataObj[i].id}. ${dataObj[i].todo}`);
-				}
-			}
-		});
-	}
-
-	static delete(id) {
-		TODOModels.readFile(function (dataObj) {
-			let deletedData = '';
-
-			for (let i = 0; i < dataObj.length; i++) {
-				if (dataObj[i].id == id) {
-					deletedData = dataObj[i].todo;
-					dataObj.splice(i, 1);
-					break;
-				}
-			}
-
-			TODOModels.writeFile(dataObj, function() {
-				TODOViews.showMessage(`deleted ${deletedData} from your TODO list...`);
-			});
-		});
-	}
-
-	static complete(id) {
-		TODOModels.readFile(function (dataObj) {
-			for (let i = 0; i < dataObj.length; i++) {
-				if (dataObj[i].id == id) {
-					dataObj[i].status = true;
-					dataObj[i].completed_date = new Date();
-					break;
-				}
-			}
-
-
-			TODOModels.writeFile(dataObj, function() {
-				TODOControls.list();
-			});
-		});
-	}
-
-	static uncomplete(id) {
-		TODOModels.readFile(function (dataObj) {
-			for (let i = 0; i < dataObj.length; i++) {
-				if (dataObj[i].id == id) {
-					dataObj[i].status = false;
-					dataObj[i].completed_date = '';
-					break;
-				}
-			}
-
-			TODOModels.writeFile(dataObj, function() {
-				TODOControls.list();
-			});
-		});
-	}
-
-	static tag(option) {
-		let id = option.split(' ')[0];
-
-		TODOModels.readFile(function (dataObj) {
-			let name_task = '';
-			let tags = [];
-
-			for (let i = 0; i < dataObj.length; i++) {
-				if (dataObj[i].id == id) {
-					name_task = dataObj[i].todo;
-					for (let j = 1; j < option.split(' ').length; j++) {
-						if (dataObj[i].tag.indexOf(option.split(' ')[j]) == -1) {
-							dataObj[i].tag.push(option.split(' ')[j]);
-							tags.push(option.split(' ')[j]);
-						}
-					}
-					break;
-				}
-			}
-
-			TODOModels.writeFile(dataObj, function() {
-				TODOViews.showMessage(`Tagged task "${name_task}" with tags: ${tags.join(' ')}`);
-			});
-		});
-	}
-
-	static filter(option) {
-		TODOModels.readFile(function (dataObj) {
-			let result = [];
-
-			for (let i = 0; i < dataObj.length; i++) {
-				for (let j = 0; j < dataObj[i].tag.length; j++) {
-
-					if (dataObj[i].tag[j] == option) {
-						result.push(dataObj[i]);
-						break;
-					}
-				}
-			}
-
-			TODOViews.showListMessage(result);
-		});
-	}
-
-	static sort(data, option, property) {
-		for (let i = 1; i < data.length; i++) {
-			for (let j = 0; j < i; j++) {
-				if (option == 'desc') {
-					if (data[i][property] > data[j][property]) {
-						let temp = data[i];
-			  			data[i] = data[j];
-			  			data[j] = temp;
-					}
-				} else {
-					if (data[i][property] < data[j][property]) {
-						let temp = data[i];
-			  			data[i] = data[j];
-			  			data[j] = temp;
-					}
-				}
-			}
-		}
 	}
 }
 
--- model.js
+++ model.js
@@ -20,6 +20,171 @@ class TODOModels {
 		  callback();
 		});
 	}
+
+	static list(option, sort, callback) {
+		TODOModels.readFile(function (dataObj) {
+			switch (option) {
+				case 'created' : {
+					TODOModels.sort(dataObj, sort, 'created_date');
+					callback(dataObj);
+					break;
+				}
+				case 'completed' : {
+					let dataObjCompleted = [];
+					for (let i = 0; i < dataObj.length; i++) {
+						if (dataObj[i].completed_date != '' && dataObj[i].status) dataObjCompleted.push(dataObj[i]);
+					}
+					TODOModels.sort(dataObjCompleted, sort, 'completed_date')
+					callback(dataObjCompleted);
+					break;
+				}
+				default : callback(dataObj);
+			}
+		});
+	}
+
+	static add(data, callback) {
+		TODOModels.readFile(function (dataObj) {
+			let newObj = {
+				id : (Number(dataObj[dataObj.length - 1].id) + 1).toString(),
+				todo : data,
+				status : false,
+				created_date : new Date(),
+				completed_date : '',
+				tag : []
+			};
+
+			dataObj.push(newObj);
+			TODOModels.writeFile(dataObj, function() {
+				callback(dataObj);
+			});
+		});
+	}
+
+	static findById(id, callback) {
+		TODOModels.readFile(function (dataObj) {
+			for (let i = 0; i < dataObj.length; i++) {
+				if (dataObj[i].id == id) {
+					callback(dataObj);
+				}
+			}
+		});
+	}
+
+	static delete(id, callback) {
+		TODOModels.readFile(function (dataObj) {
+			let deletedData = '';
+
+			for (let i = 0; i < dataObj.length; i++) {
+				if (dataObj[i].id == id) {
+					deletedData = dataObj[i].todo;
+					dataObj.splice(i, 1);
+					break;
+				}
+			}
+
+			TODOModels.writeFile(dataObj, function() {
+				callback(deletedData);
+			});
+		});
+	}
+
+	static complete(id, callback) {
+		TODOModels.readFile(function (dataObj) {
+			for (let i = 0; i < dataObj.length; i++) {
+				if (dataObj[i].id == id) {
+					dataObj[i].status = true;
+					dataObj[i].completed_date = new Date();
+					break;
+				}
+			}
+
+
+			TODOModels.writeFile(dataObj, function() {
+				callback();
+			});
+		});
+	}
+
+	static uncomplete(id, callback) {
+		TODOModels.readFile(function (dataObj) {
+			for (let i = 0; i < dataObj.length; i++) {
+				if (dataObj[i].id == id) {
+					dataObj[i].status = false;
+					dataObj[i].completed_date = '';
+					break;
+				}
+			}
+
+			TODOModels.writeFile(dataObj, function() {
+				callback();
+			});
+		});
+	}
+
+	static tag(option, callback) {
+		let id = option.split(' ')[0];
+
+		TODOModels.readFile(function (dataObj) {
+			let name_task = '';
+			let tags = [];
+
+			for (let i = 0; i < dataObj.length; i++) {
+				if (dataObj[i].id == id) {
+					name_task = dataObj[i].todo;
+					for (let j = 1; j < option.split(' ').length; j++) {
+						if (dataObj[i].tag.indexOf(option.split(' ')[j]) == -1) {
+							dataObj[i].tag.push(option.split(' ')[j]);
+							tags.push(option.split(' ')[j]);
+						}
+					}
+					break;
+				}
+			}
+
+			TODOModels.writeFile(dataObj, function() {
+				callback(name_task, tags);
+			});
+		});
+	}
+
+	static filter(option, callback) {
+		TODOModels.readFile(function (dataObj) {
+			let result = [];
+
+			for (let i = 0; i < dataObj.length; i++) {
+				for (let j = 0; j < dataObj[i].tag.length; j++) {
+
+					if (dataObj[i].tag[j] == option) {
+						result.push(dataObj[i]);
+						break;
+					}
+				}
+			}
+
+			callback(result);
+		});
+	}
+
+	static sort(data, option, property) {
+		for (let i = 1; i < data.length; i++) {
+			for (let j = 0; j < i; j++) {
+				if (option == 'desc') {
+					if (data[i][property] > data[j][property]) {
+						let temp = data[i];
+			  			data[i] = data[j];
+			  			data[j] = temp;
+					}
+				} else {
+					if (data[i][property] < data[j][property]) {
+						let temp = data[i];
+			  			data[i] = data[j];
+			  			data[j] = temp;
+					}
+				}
+			}
+		}
+	}
 }
 
 module.exports = TODOModels;
